4796973d52ac7b95655bbaf1f98c3d0272dd391f
Gabriel Budke de Sousa <38558555+gabrielbudke@users.noreply.github.com>
2018-08-02 11:31:12 -0300

    Atualização do Repositório

---
+++ WebExemplo02/src/java/br/com/entra21java/bean/AlimentoBean.java
@@ -0,0 +1,54 @@
+package br.com.entra21java.bean;
+
+/**
+ * @author Gabriel Budke
+ */
+public class AlimentoBean {
+    
+    private int id;
+    private String nome, descricao;
+    private byte quantidade;
+    private double preco;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public byte getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(byte quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    
+}
